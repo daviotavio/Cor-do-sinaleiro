060e14b5e26275bc19d2a08ff60a2e0081da8fe4
Davi Otavio <85647007+daviotavio@users.noreply.github.com>
2021-09-09 18:08:05 -0300

    Add files via upload

---
+++ verde vermelho amarelo/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace verde_vermelho_amarelo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Desenvolva um código que pergunte “Qual é a cor (VERDE/AMARELO/VERMELHO)?”. Se for escolhida a cor VERDE, imprima “Siga em frente”; se for escolhida a opção AMARELO, imprima “Atenção”; e, se for escolhido VERMELHO, imprima “Pare”. 
+
+            string cor = "";
+
+            Console.Write("Qual a cor do sinal? (verde, amarelo, vermelho): ");
+            cor = Console.ReadLine();
+
+            switch (cor)
+            {
+            case "verde":
+                Console.Write("Siga em frente");
+                    break;
+
+            case "amarelo":
+                Console.Write("Pare!");
+                    break;
+
+            case "vermelho":
+                Console.Write("Atenção!");
+                    break;
+
+            default: 
+                Console.WriteLine("Escolha somente entre as cores 'verde, vermelho e amarelo'.");
+                    break;
+            }
+        }
+    }
+}
